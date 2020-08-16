0a63450cdc6f3bfc73525c8fc2f4b92199c38e31
Carlos Andres Aguilar Nieto <caguilar@ec.krugercorp.com>
2020-08-16 12:43:49 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ApiTestService} from '../services/api-test.service';
+import {User} from "../model/user";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-test-httpclient';
+
+  constructor(private apiTestService: ApiTestService) {
+  }
+
+  ngOnInit() {
+  }
+
+  createUser() {
+
+    const user = new User()
+
+    user.firstName = "test"
+    user.lastName = "lastname"
+
+    this.apiTestService.createUser(user).subscribe((res)=>{
+      console.log(res);
+    });
+  }
+
+  getUsers() {
+    this.apiTestService.getUsers().subscribe((res)=>{
+      console.log(res);
+    });
+  }
+
+
 }
