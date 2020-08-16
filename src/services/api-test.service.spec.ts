825d7cde4717664792b148ece779e3c1f6a63257
Carlos Andres Aguilar Nieto <caguilar@ec.krugercorp.com>
2020-08-16 16:31:13 -0500

    fix test

--- src/services/api-test.service.spec.ts
+++ src/services/api-test.service.spec.ts
@@ -1,12 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiTestService } from './api-test.service';
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 
-describe('ApiTestService', () => {
+fdescribe('ApiTestService', () => {
   let service: ApiTestService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        CommonModule,
+        FormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
+    });
     service = TestBed.inject(ApiTestService);
   });
 
